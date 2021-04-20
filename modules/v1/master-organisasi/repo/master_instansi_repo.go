d6d7acde52973601b7e70a35bd62c52c0f757509
Ega Evan <ega.evan@uii.ac.id>
2021-04-20 22:03:37 +0700

    Edit endpoint instansi

--- modules/v1/master-organisasi/repo/master_instansi_repo.go
+++ modules/v1/master-organisasi/repo/master_instansi_repo.go
@@ -6,18 +6,15 @@ import (
 	"svc-insani-go/modules/v1/master-organisasi/model"
 )
 
-func SearchInstansi(a app.App, ctx context.Context, nama string) (*model.Instansi, error) {
+func SearchInstansi(a app.App, ctx context.Context) ([]model.Instansi, error) {
 
-	var instansi model.Instansi
+	var instansi []model.Instansi
 	tx := a.GormDB.WithContext(ctx)
 
-	if nama != "" {
-		res := tx.Where("nama_instansi LIKE ? OR kd_instansi LIKE ?", "%"+nama+"%", "%"+nama+"%").
-			First(&instansi)
-		if res.Error != nil {
-			return nil, res.Error
-		}
+	res := tx.Find(&instansi)
+	if res.Error != nil {
+		return nil, res.Error
 	}
 
-	return &instansi, nil
+	return instansi, nil
 }
