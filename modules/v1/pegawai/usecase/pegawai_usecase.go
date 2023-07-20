8119eee6779bad10b979bf8bed71e63540d0cac8
Amalia Citra <amalia.citra@uii.ac.id>
2023-07-20 13:20:01 +0700

    update function

--- modules/v1/pegawai/usecase/pegawai_usecase.go
+++ modules/v1/pegawai/usecase/pegawai_usecase.go
@@ -374,7 +374,7 @@ func HandleGetPegawaiByNik(a *app.App) echo.HandlerFunc {
 }
 
 // ini digunakan kalo uiitanggungan sudah digunakan
-func HandleGetPegawaiPrivate_v1(a *app.App, public bool) echo.HandlerFunc {
+func HandleGetPegawaiPrivateSatu(a *app.App, public bool) echo.HandlerFunc {
 	h := func(c echo.Context) error {
 		env := os.Getenv("ENV")
 		fmt.Println(env)
@@ -578,7 +578,7 @@ func HandleGetPegawaiPrivate_v1(a *app.App, public bool) echo.HandlerFunc {
 			pegawaiJabfungJabstrukAndKontrakAndPendidikan = append(pegawaiJabfungJabstrukAndKontrakAndPendidikan, data)
 		}
 
-		tanggunganResponse := GetDataTanggungan_v1(public)
+		tanggunganResponse := GetDataTanggunganSatu(public)
 
 		var pegawaiJabfungJabstrukAndKontrakAndPendidikanAndTangungan []model.PegawaiPrivate
 		for _, data := range pegawaiJabfungJabstrukAndKontrakAndPendidikan {
@@ -607,7 +607,7 @@ func HandleGetPegawaiPrivate_v1(a *app.App, public bool) echo.HandlerFunc {
 }
 
 // ini digunakan kalo uiitanggungan sudah digunakan
-func GetDataTanggungan_v1(public bool) *model.TanggunganResponseBody {
+func GetDataTanggunganSatu(public bool) *model.TanggunganResponseBody {
 	// fmt.Println(env)
 	var baseURL string
 	baseURL = os.Getenv("URL_HCM_TANGGUNGAN")
